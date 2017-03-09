ee1a59414f919cfe196c1938f5bc81610c279491
Despo <adrien.aresu@gmail.com>
2017-03-10 00:02:04 +0100

    Troisieme Push
    a+ Particule Balles
    + Destruction Ennemies
    + Caméra
    + Debut Shop
    + Bouton Tourelle Standard
    + Node Effects

---
+++ TowerDefense3D/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+public class BuildManager : MonoBehaviour {
+
+	//Rend accessible sans référence pour les nodes :
+	public static BuildManager instance;
+
+
+	void Awake ()
+	{
+		if (instance != null) 
+		{
+			Debug.LogError ("PLus d'un BuildManager est lancé");
+			return;
+		}
+		instance = this;
+
+	}
+
+	public GameObject standardTurretPrefab;
+
+	void Start()
+	{
+		turretToBuild = standardTurretPrefab;
+	}
+
+
+	private GameObject turretToBuild;
+
+
+	public GameObject GetTurretToBuild()
+	{
+		return turretToBuild;
+	}
+
+
+
+}
--- TowerDefense3D/Assets/Scripts/Bullet.cs
+++ TowerDefense3D/Assets/Scripts/Bullet.cs
@@ -3,8 +3,9 @@
 public class Bullet : MonoBehaviour {
 
 	private Transform target;
-	public float speed = 70f;
 
+	public float speed = 70f;
+	public GameObject impactEffect;
 	public void Seek (Transform _target)
 	{
 
@@ -37,6 +38,11 @@ public class Bullet : MonoBehaviour {
 
 	void HitTarget()
 	{
+		GameObject effectIns = (GameObject) Instantiate (impactEffect, transform.position, transform.rotation);
+		Destroy (effectIns, 2f);
+
+		Destroy (target.gameObject);
+
 		Destroy(gameObject);
 		return;
 	}
